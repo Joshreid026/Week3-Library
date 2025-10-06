1fa9c1164aab48c952ecd1732b51e5ab6720bf0c
Josh Reid <Reid-J55@ulster.ac.uk>
2025-10-06 15:11:08 +0100

    Updated code with access modifiers

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,31 +1,13 @@
-﻿class Book
+﻿using Library;
+
+class Program
 {
-    string Title;
-    string Author;
-    string ISBN;
-
-    public Book(string title, string author, string iSBN)
-    {
-        Title = title;
-        Author = author;
-        ISBN = iSBN;
-    }
-
-    void DisplayInfo()
-    {
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-    }
-
     static void Main(string[] args)
     {
         // Create a new instance (object) of the Book class
         // Note how the object name differs from the class name
-        Book book = new Book("C# for beginners", "BillGates", "12345678");
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
 
-        // Output book information to the console
         book.DisplayInfo();
     }
 }
