1366e87caf15a4961b077da8fe4d02843ee3a7f3
Josh Reid <Reid-J55@ulster.ac.uk>
2025-10-06 15:20:12 +0100

    Updated code with variables and properties

--- Library/Book.cs
+++ Library/Book.cs
@@ -2,22 +2,40 @@
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title;
+        private string author;
+        private string iSBN;
+
+        // Title property to allow access
+        // to the title private variable
+        public string Title
+        {
+            get { return title; }  // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return iSBN; }
+            set { iSBN = value; }
+        }
 
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            title = bookTitle;
+            author = bookAuthor;
+            iSBN = bookISBN;
         }
 
         public void DisplayInfo()
         {
-            Console.WriteLine($"Book title: {Title}");
-            Console.WriteLine($"Book Author: {Author}");
-            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine($"Book title: {title}");
+            Console.WriteLine($"Book Author: {author}");
+            Console.WriteLine($"Book ISBN: {iSBN}");
             Console.WriteLine();
         }
     }
